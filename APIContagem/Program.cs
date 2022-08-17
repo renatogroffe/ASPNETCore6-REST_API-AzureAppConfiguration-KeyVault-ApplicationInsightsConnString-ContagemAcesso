451c65211f2a4aba7827e003721a732a1ac25354
Renato Groffe <renato.groffe@yahoo.com.br>
2022-08-17 11:58:34 -0300

    .NET 6 + ASP.NET Core + API REST (Contagem de Acessos) + Application Insights (via Connection String) + Azure App Configuration + Azure Key Vault + Dockerfile + Linux + Contagem de acessos e testes com mensagens
    .NET 6 + ASP.NET Core + API REST (Contagem de Acessos) + Application Insights (via Connection String) + Azure App Configuration + Azure Key Vault + Dockerfile + Linux + Contagem de acessos e testes com mensagens

---
+++ APIContagem/Program.cs
@@ -0,0 +1,57 @@
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionAppConfiguration =
+    builder.Configuration.GetConnectionString("AppConfiguration");
+var useAppConfiguration =
+    !String.IsNullOrWhiteSpace(connectionAppConfiguration);
+
+if (useAppConfiguration)
+{
+    builder.Host.ConfigureAppConfiguration(cfg => {
+        cfg.AddAzureAppConfiguration(options =>
+        {
+            options.Connect(connectionAppConfiguration)
+                .ConfigureRefresh(refresh =>
+                {
+                    refresh.Register("Mensagens:Aviso").SetCacheExpiration(
+                        TimeSpan.FromSeconds(20));
+                });
+            
+            if (Convert.ToBoolean(builder.Configuration["UsingAzureKeyVault"]))
+                options.ConfigureKeyVault(kv =>
+                    {
+                        kv.SetCredential(new DefaultAzureCredential());
+                    });
+        });
+    });
+
+    builder.Services.AddAzureAppConfiguration();
+}
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+if (!String.IsNullOrWhiteSpace(builder.Configuration.GetConnectionString("ApplicationInsights")))
+    builder.Services.AddApplicationInsightsTelemetry(options =>
+    {
+        options.ConnectionString = builder.Configuration.GetConnectionString("ApplicationInsights");
+    });
+
+var app = builder.Build();
+
+if (useAppConfiguration)
+    app.UseAzureAppConfiguration();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ASPNETCore6-REST_API-AzureAppConfiguration-KeyVault-ApplicationInsightsConnString-ContagemAcesso
 Exemplo de API REST para contagem de acessos criada com .NET 6 + ASP.NET Core e que possibilita o monitoramento com o Application Insights (via Connection String), utilizando (opcionalmente) configurações com conteúdos de mensagens armazenadas em um recurso do Azure App Configuration e no Azure Key Vault. Inclui um Dockerfile para a geração de imagens baseadas em Linux.
+
+Referência:
+
+[**Configure Applications with App Configuration and Key Vault | Microsoft Docs**](https://docs.microsoft.com/en-us/samples/azure/azure-sdk-for-net/app-secrets-configuration/?tabs=visualstudio)
